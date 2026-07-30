83725032d3a9f40e9b69f4034492995f41f6005b
Onaindini <onai.sanz@gmail.com>
2026-07-30 21:23:58 +0200

    removed leftover conflict from combo box fix

--- TrailGuard/EditTrailForm.cs
+++ TrailGuard/EditTrailForm.cs
@@ -197,11 +197,7 @@ namespace TrailGuard
                 SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT TrailID, TrailName FROM Trail WHERE ParkID = " + parkId, conn);
                 DataTable dataTableTrail = new DataTable();
                 dataAdapter.Fill(dataTableTrail);
-<<<<<<< HEAD
 
-=======
-                
->>>>>>> ad0bf67 (Updated build no unsaved work)
                 comboBoxSelectTrail.DisplayMember = "TrailName";
                 comboBoxSelectTrail.ValueMember = "TrailID";
                 comboBoxSelectTrail.DataSource = dataTableTrail;
