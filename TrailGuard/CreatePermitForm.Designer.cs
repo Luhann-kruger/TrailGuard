224652c983265f68c1c2164b7f348bd270468537
Luhann-kruger <luhannkruger@gmail.com>
2026-08-17 10:01:50 +0200

    Impletement UI and loading of data of create Permit form

--- TrailGuard/CreatePermitForm.Designer.cs
+++ TrailGuard/CreatePermitForm.Designer.cs
@@ -31,8 +31,25 @@
             pnlHeader = new Panel();
             lblModeTitle = new Label();
             lblTitle = new Label();
-            lblCreateTrailTitle = new Label();
+            lblCreatePermitTitle = new Label();
+            cmbSelectPark = new ComboBox();
+            lblSelectPark = new Label();
+            lblSelectTrail = new Label();
+            lblSelectDate = new Label();
+            lblExpectedReturnTime = new Label();
+            cmbSelectTrail = new ComboBox();
+            dtpPermitDate = new DateTimePicker();
+            dtpExpectedReturnTime = new DateTimePicker();
+            pnlAddParticipants = new Panel();
+            label1 = new Label();
+            dgvParticipants = new DataGridView();
+            btnSearchParticipant = new ReaLTaiizor.Controls.ForeverButton();
+            txtSearchParticipant = new TextBox();
+            lblAddParticipantHeading = new Label();
+            btnCreatePermit = new ReaLTaiizor.Controls.ForeverButton();
             pnlHeader.SuspendLayout();
+            pnlAddParticipants.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvParticipants).BeginInit();
             SuspendLayout();
             // 
             // pnlHeader
@@ -67,28 +84,196 @@
             lblTitle.TabIndex = 1;
             lblTitle.Text = "TrailGuard";
             // 
-            // lblCreateTrailTitle
+            // lblCreatePermitTitle
             // 
-            lblCreateTrailTitle.AutoSize = true;
-            lblCreateTrailTitle.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblCreateTrailTitle.Location = new Point(12, 95);
-            lblCreateTrailTitle.Name = "lblCreateTrailTitle";
-            lblCreateTrailTitle.Size = new Size(173, 40);
-            lblCreateTrailTitle.TabIndex = 4;
-            lblCreateTrailTitle.Text = "Create Trail";
+            lblCreatePermitTitle.AutoSize = true;
+            lblCreatePermitTitle.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCreatePermitTitle.Location = new Point(12, 95);
+            lblCreatePermitTitle.Name = "lblCreatePermitTitle";
+            lblCreatePermitTitle.Size = new Size(205, 40);
+            lblCreatePermitTitle.TabIndex = 4;
+            lblCreatePermitTitle.Text = "Create Permit";
+            // 
+            // cmbSelectPark
+            // 
+            cmbSelectPark.FormattingEnabled = true;
+            cmbSelectPark.Location = new Point(136, 164);
+            cmbSelectPark.Name = "cmbSelectPark";
+            cmbSelectPark.Size = new Size(229, 23);
+            cmbSelectPark.TabIndex = 5;
+            cmbSelectPark.SelectedIndexChanged += cmbSelectPark_SelectedIndexChanged;
+            // 
+            // lblSelectPark
+            // 
+            lblSelectPark.AutoSize = true;
+            lblSelectPark.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSelectPark.Location = new Point(12, 166);
+            lblSelectPark.Name = "lblSelectPark";
+            lblSelectPark.Size = new Size(99, 21);
+            lblSelectPark.TabIndex = 6;
+            lblSelectPark.Text = "Select Park:";
+            // 
+            // lblSelectTrail
+            // 
+            lblSelectTrail.AutoSize = true;
+            lblSelectTrail.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSelectTrail.Location = new Point(476, 168);
+            lblSelectTrail.Name = "lblSelectTrail";
+            lblSelectTrail.Size = new Size(97, 21);
+            lblSelectTrail.TabIndex = 7;
+            lblSelectTrail.Text = "Select Trail:";
+            // 
+            // lblSelectDate
+            // 
+            lblSelectDate.AutoSize = true;
+            lblSelectDate.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblSelectDate.Location = new Point(14, 245);
+            lblSelectDate.Name = "lblSelectDate";
+            lblSelectDate.Size = new Size(100, 21);
+            lblSelectDate.TabIndex = 8;
+            lblSelectDate.Text = "Select Date:";
+            // 
+            // lblExpectedReturnTime
+            // 
+            lblExpectedReturnTime.AutoSize = true;
+            lblExpectedReturnTime.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblExpectedReturnTime.Location = new Point(476, 245);
+            lblExpectedReturnTime.Name = "lblExpectedReturnTime";
+            lblExpectedReturnTime.Size = new Size(181, 21);
+            lblExpectedReturnTime.TabIndex = 9;
+            lblExpectedReturnTime.Text = "Expected Return Time:";
+            // 
+            // cmbSelectTrail
+            // 
+            cmbSelectTrail.FormattingEnabled = true;
+            cmbSelectTrail.Location = new Point(647, 168);
+            cmbSelectTrail.Name = "cmbSelectTrail";
+            cmbSelectTrail.Size = new Size(229, 23);
+            cmbSelectTrail.TabIndex = 10;
+            cmbSelectTrail.SelectedIndexChanged += cmbSelectTrail_SelectedIndexChanged;
+            // 
+            // dtpPermitDate
+            // 
+            dtpPermitDate.Location = new Point(136, 245);
+            dtpPermitDate.Name = "dtpPermitDate";
+            dtpPermitDate.Size = new Size(200, 23);
+            dtpPermitDate.TabIndex = 11;
+            dtpPermitDate.ValueChanged += dtpPermitDate_ValueChanged;
+            // 
+            // dtpExpectedReturnTime
+            // 
+            dtpExpectedReturnTime.CustomFormat = "HH:mm:ss";
+            dtpExpectedReturnTime.Format = DateTimePickerFormat.Custom;
+            dtpExpectedReturnTime.Location = new Point(676, 244);
+            dtpExpectedReturnTime.Name = "dtpExpectedReturnTime";
+            dtpExpectedReturnTime.ShowUpDown = true;
+            dtpExpectedReturnTime.Size = new Size(200, 23);
+            dtpExpectedReturnTime.TabIndex = 12;
+            dtpExpectedReturnTime.ValueChanged += dtpExpectedReturnTime_ValueChanged;
+            // 
+            // pnlAddParticipants
+            // 
+            pnlAddParticipants.BackColor = Color.White;
+            pnlAddParticipants.Controls.Add(label1);
+            pnlAddParticipants.Controls.Add(dgvParticipants);
+            pnlAddParticipants.Controls.Add(btnSearchParticipant);
+            pnlAddParticipants.Controls.Add(txtSearchParticipant);
+            pnlAddParticipants.Controls.Add(lblAddParticipantHeading);
+            pnlAddParticipants.Location = new Point(14, 321);
+            pnlAddParticipants.Name = "pnlAddParticipants";
+            pnlAddParticipants.Size = new Size(953, 293);
+            pnlAddParticipants.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(482, 15);
+            label1.TabIndex = 14;
+            label1.Text = "Please Note: You should first register a participant (hiker) before you can add to the permit\r\n";
+            label1.Click += label1_Click;
+            // 
+            // dgvParticipants
+            // 
+            dgvParticipants.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvParticipants.Location = new Point(18, 88);
+            dgvParticipants.Name = "dgvParticipants";
+            dgvParticipants.Size = new Size(906, 187);
+            dgvParticipants.TabIndex = 14;
+            dgvParticipants.CellContentClick += dgvParticipants_CellContentClick;
+            // 
+            // btnSearchParticipant
+            // 
+            btnSearchParticipant.BackColor = Color.Transparent;
+            btnSearchParticipant.BaseColor = Color.FromArgb(44, 123, 102);
+            btnSearchParticipant.Font = new Font("Segoe UI", 12F);
+            btnSearchParticipant.Location = new Point(805, 25);
+            btnSearchParticipant.Name = "btnSearchParticipant";
+            btnSearchParticipant.Rounded = false;
+            btnSearchParticipant.Size = new Size(119, 29);
+            btnSearchParticipant.TabIndex = 11;
+            btnSearchParticipant.Text = "Search";
+            btnSearchParticipant.TextColor = Color.FromArgb(243, 243, 243);
+            // 
+            // txtSearchParticipant
+            // 
+            txtSearchParticipant.BackColor = SystemColors.ScrollBar;
+            txtSearchParticipant.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtSearchParticipant.Location = new Point(556, 25);
+            txtSearchParticipant.Name = "txtSearchParticipant";
+            txtSearchParticipant.Size = new Size(230, 27);
+            txtSearchParticipant.TabIndex = 10;
+            // 
+            // lblAddParticipantHeading
+            // 
+            lblAddParticipantHeading.AutoSize = true;
+            lblAddParticipantHeading.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAddParticipantHeading.Location = new Point(18, 14);
+            lblAddParticipantHeading.Name = "lblAddParticipantHeading";
+            lblAddParticipantHeading.Size = new Size(162, 25);
+            lblAddParticipantHeading.TabIndex = 9;
+            lblAddParticipantHeading.Text = "Add Participant: ";
+            // 
+            // btnCreatePermit
+            // 
+            btnCreatePermit.BackColor = Color.Transparent;
+            btnCreatePermit.BaseColor = Color.FromArgb(44, 123, 102);
+            btnCreatePermit.Font = new Font("Segoe UI", 12F);
+            btnCreatePermit.Location = new Point(848, 644);
+            btnCreatePermit.Name = "btnCreatePermit";
+            btnCreatePermit.Rounded = false;
+            btnCreatePermit.Size = new Size(119, 29);
+            btnCreatePermit.TabIndex = 14;
+            btnCreatePermit.Text = "Create Permit";
+            btnCreatePermit.TextColor = Color.FromArgb(243, 243, 243);
             // 
             // CreatePermitForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ScrollBar;
-            ClientSize = new Size(1004, 590);
-            Controls.Add(lblCreateTrailTitle);
+            ClientSize = new Size(1004, 685);
+            Controls.Add(btnCreatePermit);
+            Controls.Add(pnlAddParticipants);
+            Controls.Add(dtpExpectedReturnTime);
+            Controls.Add(dtpPermitDate);
+            Controls.Add(cmbSelectTrail);
+            Controls.Add(lblExpectedReturnTime);
+            Controls.Add(lblSelectDate);
+            Controls.Add(lblSelectTrail);
+            Controls.Add(lblSelectPark);
+            Controls.Add(cmbSelectPark);
+            Controls.Add(lblCreatePermitTitle);
             Controls.Add(pnlHeader);
             Name = "CreatePermitForm";
             Text = "CreatePermitForm";
+            Load += CreatePermitForm_Load;
             pnlHeader.ResumeLayout(false);
             pnlHeader.PerformLayout();
+            pnlAddParticipants.ResumeLayout(false);
+            pnlAddParticipants.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvParticipants).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -98,6 +283,21 @@
         private Panel pnlHeader;
         private Label lblModeTitle;
         private Label lblTitle;
-        private Label lblCreateTrailTitle;
+        private Label lblCreatePermitTitle;
+        private ComboBox cmbSelectPark;
+        private Label lblSelectPark;
+        private Label lblSelectTrail;
+        private Label lblSelectDate;
+        private Label lblExpectedReturnTime;
+        private ComboBox cmbSelectTrail;
+        private DateTimePicker dtpPermitDate;
+        private DateTimePicker dtpExpectedReturnTime;
+        private Panel pnlAddParticipants;
+        private Label lblAddParticipantHeading;
+        private TextBox txtSearchParticipant;
+        private ReaLTaiizor.Controls.ForeverButton btnSearchParticipant;
+        private DataGridView dgvParticipants;
+        private Label label1;
+        private ReaLTaiizor.Controls.ForeverButton btnCreatePermit;
     }
 }
--- TrailGuard/CreatePermitForm.cs
+++ TrailGuard/CreatePermitForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,266 @@ namespace TrailGuard
 {
     public partial class CreatePermitForm : Form
     {
+        int parkID;
+        int trailID;
+        private DateTime selectedDate;
+        private TimeSpan expectedReturnTime;
+
+        //List to  store all the selected participant's Id
+        private List<int> selectedParticpantIds = new List<int>();
+
+
+        string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=TrailGuardDB;Integrated Security=True;";
+
+
+        SqlConnection conn;
         public CreatePermitForm()
         {
             InitializeComponent();
+        }
+
+        private void CreatePermitForm_Load(object sender, EventArgs e)
+        {
+            LoadParks();
+
+            //Initialize the selected date to today
+            selectedDate = DateTime.Today;
+
+            loadParticipants();
+
+            styleDGVParticipants();
+
+            //Load default selections for each participant
+            SetInitialBtnActionValues();
+
+            txtSearchParticipant.PlaceholderText = "Search a participant by first name, last name or participant ID...";
+        }
+
+        //function that add an actions column with action buttons
+        private void AddActionColumn()
+        {
+
+            if (dgvParticipants.Columns.Contains("Actions"))
+            {
+                return;
+            }
+
+            DataGridViewButtonColumn btnColumn = new DataGridViewButtonColumn();
+            btnColumn.Name = "Actions";
+            btnColumn.HeaderText = "Actions";
+            //btnColumn.Text = "Select Action";
+            btnColumn.UseColumnTextForButtonValue = false;
+
+            //default color styling of the action buttons
+            //btnColumn.DefaultCellStyle.BackColor = Color.FromArgb(6, 71, 96);
+            btnColumn.DefaultCellStyle.ForeColor = Color.White;
+
+            dgvParticipants.Columns.Add(btnColumn);
+        }
+
+        //Populate the Actions button column with "Add" or "Remove" whether the value is in the list or not
+        private void SetInitialBtnActionValues() {
+            foreach (DataGridViewRow row in dgvParticipants.Rows) {
+                if (row.IsNewRow) continue;
+
+                int participantId = Convert.ToInt32(row.Cells["ParticipantID"].Value);
+
+                if (selectedParticpantIds.Contains(participantId))
+                {
+                    row.Cells["Actions"].Value = "Remove";
+                    row.DefaultCellStyle.BackColor = Color.LightGreen;
+
+                }
+                else {
+                    row.Cells["Actions"].Value = "Add";
+                    row.DefaultCellStyle.BackColor = Color.White;
+                }
+            }
+        }
+
+        //Load all the registered participants into the dgvParticipants 
+        private void loadParticipants()
+        {
+            try
+            {
+                conn = new SqlConnection(connString);
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                string sqlQuery = @"SELECT ParticipantID,FirstName, LastName, IDNumber, PhoneNumber FROM Participant";
+                SqlCommand comm = new SqlCommand(sqlQuery, conn);
+                DataTable dt = new DataTable();
+
+                adapter.SelectCommand = comm;
+                adapter.Fill(dt);
+                dgvParticipants.DataSource = dt;
+
+                //Load Action columns
+                AddActionColumn();
+
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //function to style the datagrid view
+        private void styleDGVParticipants()
+        {
+            dgvParticipants.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvParticipants.EnableHeadersVisualStyles = false;
+            dgvParticipants.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 191, 99);
+            dgvParticipants.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
+            dgvParticipants.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            dgvParticipants.RowTemplate.Height = 30;
+        }
+
+        //function that loads the parks into cmbSelectPark
+        private void LoadParks()
+        {
+            try
+            {
+                conn = new SqlConnection(connString);
+                conn.Open();
+                string sqlQuery = "SELECT ParkID, ParkName FROM Park ORDER BY ParkName";
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, conn);
+
+                adapter.Fill(dt);
+
+                cmbSelectPark.DisplayMember = "ParkName";
+                cmbSelectPark.ValueMember = "ParkID";
+                cmbSelectPark.DataSource = dt;
+
+
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error loading parks: " + ex.Message);
+            }
+        }
+
+        private void LoadTrails()
+        {
+            try
+            {
+                conn = new SqlConnection(connString);
+                conn.Open();
+                string sqlQuery = "SELECT TrailID, TrailName FROM Trail WHERE ParkID = @parkID";
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                cmd.Parameters.AddWithValue("@parkID", parkID);
+
+
+                adapter.SelectCommand = cmd;
+
+
+                adapter.Fill(dt);
+
+
+
+                cmbSelectTrail.DisplayMember = "TrailName";
+                cmbSelectTrail.ValueMember = "TrailID";
+                cmbSelectTrail.DataSource = dt;
+
+
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error loading Trails: " + ex.Message);
+            }
+        }
+
+        private void cmbSelectPark_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSelectPark.SelectedValue != null && cmbSelectPark.SelectedValue != DBNull.Value)
+            {
+                parkID = Convert.ToInt32(cmbSelectPark.SelectedValue);
+
+                // load trails only if a park was selected
+                if (parkID >= 0)
+                {
+                    LoadTrails();
+                }
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbSelectTrail_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSelectTrail.SelectedValue != null)
+            {
+                trailID = Convert.ToInt32(cmbSelectTrail.SelectedValue);
+
+            }
+        }
+
+        private void dtpPermitDate_ValueChanged(object sender, EventArgs e)
+        {
+            selectedDate = dtpPermitDate.Value.Date;
+        }
+
+        private void dtpExpectedReturnTime_ValueChanged(object sender, EventArgs e)
+        {
+            expectedReturnTime = dtpExpectedReturnTime.Value.TimeOfDay;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvParticipants_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) { 
+            return; }
+            // NOTE PROGRAM STILL CRASH WHEN I CLICK IN THE LAST ACTION COLUMN CELL WITHOUT A BUTTON
+            if (e.ColumnIndex == dgvParticipants.Columns["Actions"].Index) {
+                int participantId = Convert.ToInt32(dgvParticipants.Rows[e.RowIndex].Cells["ParticipantID"].Value);
+                DataGridViewButtonCell btnCell = (DataGridViewButtonCell)dgvParticipants.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+                if (!selectedParticpantIds.Contains(participantId))
+                {
+
+                    //Add Action
+                    selectedParticpantIds.Add(participantId);
+                    btnCell.Value = "Remove";
+
+                    //Style the selected row color
+                    dgvParticipants.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightGreen;
+
+                    //style the button
+                    btnCell.Style.BackColor = Color.FromArgb(198, 40, 40);  // Red BG
+                    btnCell.Style.ForeColor = Color.FromArgb(255, 255, 255); // White Text
+
+                }
+                else {
+                    //Remove Action
+                    selectedParticpantIds.Remove(participantId);
+                    btnCell.Value = "Add";
+
+                    //Style the selected row color
+                    dgvParticipants.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.White;
+
+                    //style the button
+                    btnCell.Style.BackColor = Color.FromArgb(46, 125, 50);   // Green BG
+                    btnCell.Style.ForeColor = Color.FromArgb(255, 255, 255); // White Text
+                }
+            }
         }
     }
 }
